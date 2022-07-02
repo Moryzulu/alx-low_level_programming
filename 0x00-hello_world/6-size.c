7bbcb5c76eee58d172cdc268397fe2f15f7bf705
moryzulu <morymacho6@gmail.com>
2022-07-02 05:51:22 -0700

    betty code adjustment to file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,10 +9,10 @@ int main(void)
 	char d;
 	float e;
 
-	printf("size of an int: %lu byte(s)\n" (unsigned long)sizeof(a));
-	printf("size of a char: %lu byte(s)\n" (unsigned long)sizeof(d));
-	printf("size of a long int: %lu byte(s)\n" (unsigned long)sizeof(b));
-	printf("size of a long long int: %lu byte(s)\n" (unsigned long)sizeof(c));
-	printf("size of a float: %lu byte(s)\n" (unsigned long)sizeof(e));
+	printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
 	return (0);
 }

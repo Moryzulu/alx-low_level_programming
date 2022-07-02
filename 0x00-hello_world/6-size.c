a6c441cf4f1665541f158fd4a5e06a66ead52205
moryzulu <morymacho6@gmail.com>
2022-07-02 03:37:10 -0700

    create a script that outputs data type binary numbers

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/* creating code that outputs binary numbers */
+int main(void)
+{
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float e;
+
+	printf("size of an int: %lu byte(s)\n" (unsigned long)sizeof(a));
+	printf("size of a char: %lu byte(s)\n" (unsigned long)sizeof(d));
+	printf("size of a long int: %lu byte(s)\n" (unsigned long)sizeof(b));
+	printf("size of a long long int: %lu byte(s)\n" (unsigned long)sizeof(c));
+	printf("size of a float: %lu byte(s)\n" (unsigned long)sizeof(e));
+	return (0);
+}

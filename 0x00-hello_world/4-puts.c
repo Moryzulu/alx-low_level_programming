d77df4e0c6bff4b9692164c0456d7e06c8fe70a9
moryzulu <morymacho6@gmail.com>
2022-07-02 02:31:40 -0700

    print script using printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,8 @@
  * this code prints a string and i an usung the command puts instead of printf
  */
 #include <stdio.h>
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle.\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }	
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+/** 
+ * my second string for c programming
+ * this one prints the string using the printf command
+ */
+#include <stdio.h>
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

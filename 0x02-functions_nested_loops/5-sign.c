4e4dd4d2358f04df5c896713a01c2b628596f28d
moryzulu <morymacho6@gmail.com>
2022-07-06 13:25:17 -0700

    compute absolute value

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,17 +8,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar(13);
+		_putchar(43);
 		return (1);
 	}
 	else if (n < 0)
 	{
-		_putchar(15);
+		_putchar(45);
 		return (-1);
 	}
 	else
 	{
-		_putchar(17);
+		_putchar(48);
 		return (0);
 	}
 }

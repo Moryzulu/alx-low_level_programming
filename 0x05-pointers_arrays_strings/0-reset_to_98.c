41737c37e90710b0e0d6f189eadf98d78970fb9e
moryzulu <morymacho6@gmail.com>
2022-07-12 00:27:03 -0700

    assignt pointer to value n

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
- * main- create a pointer
- * Description:value of n must be stored in pointer 
- * Return: void
- */
+  * reset_to_98 - Updates the value it points to 98
+  * @n: pointer to n
+  *
+  * Return: void
+  */
 void reset_to_98(int *n)
 {
 	*n = 98;

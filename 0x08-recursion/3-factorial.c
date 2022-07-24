c296ba7e644f3d4606f7d806f43a079c019a2c54
morymacho <morymacho6@gmail.com>
2022-07-24 14:24:58 -0700

    recursion files

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,19 +1,17 @@
 #include "main.h"
 /**
-  * factorial- Calculate factorial
-  * @n: the parameter for factoral
+  * factorial - Calculate the factorial of a number
+  * @n: the number to calculate the factorial
   *
   * Return: integer value
   */
 int factorial(int n)
-{	
+{
 	if (n < 0)
-	{
 		return (-1);
-	}
+
 	if (n <= 1)
-	{
-	return (1);
-	}
+		return (1);
+
 	return (n * factorial(n - 1));
 }

003df3010cceb89cd0ca08d68d9c29faa0b13631
moryzulu <morymacho6@gmail.com>
2022-08-01 15:37:04 -0700

    define macro

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,9 +1,19 @@
 #include<stdio.h>
-void print_filename()
+/**
+  * print_filename - Prints the file name of the program
+  *
+  * Return: Always zero
+  */
+void print_filename(void)
 {
 	printf("%s\n", __FILE__);
 }
-int main()
+/**
+  * main - Prints the file name of the program
+  *
+  * Return: Always zero
+  */
+int main(void)
 {
 	print_filename();
 	return (0);

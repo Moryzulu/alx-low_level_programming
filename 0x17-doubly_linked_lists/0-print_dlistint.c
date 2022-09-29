c9940f949dd9e64eafa22fe69b13f460ddbb9e7c
moryzulu <morymacho6@gmail.com>
2022-09-29 13:50:11 -0700

    print data in nodes

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_dlistint - function to print data of a list
+ * @dlistint_t: argument for nodes
+ *
+ * Return: length of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int len = 0;
+
+	while(h != 0)
+	{
+		printf("%d\n", h->n);
+		h = h->next;	
+		++len;
+
+	}
+	return(len);
+}
+

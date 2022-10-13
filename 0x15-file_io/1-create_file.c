cd3f1bb87d576aa0c1bb2d2891ab1bf4d6a24c45
moryzulu <morymacho6@gmail.com>
2022-10-13 13:08:29 -0700

    file input out put tasks

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,7 +27,7 @@ int create_file(const char *filename, char *text_content)
 
 /**
   * _strlen - Returns the length of a string
-  * @s: String to count
+  * @str: String to count
   *
   * Return: String length
   */
